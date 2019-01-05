b82ea08ba6f721d6e01979bb77e912085a85abee
RameesKasim <rameeschuthomandy@gmail.com>
2019-01-05 18:35:22 +0530

    created home url and view 1

--- HIM_OSF/home/views.py
+++ HIM_OSF/home/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class indexView(TemplateView):
+    template_name = "home/index.html"
