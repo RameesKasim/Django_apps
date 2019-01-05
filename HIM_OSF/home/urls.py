c130711d632110d2dae37cff65efedfab93d66c3
RameesKasim <rameeschuthomandy@gmail.com>
2019-01-05 22:47:54 +0530

    static file not working

--- HIM_OSF/home/urls.py
+++ HIM_OSF/home/urls.py
@@ -1,5 +1,7 @@
 from home.views import indexView
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
      path('', indexView.as_view(), name='index' )
  ]
